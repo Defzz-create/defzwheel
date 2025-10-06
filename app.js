56a198c432f1e4ffc6121ef448ef127fae2a22ee
Defzz-create <133031517+Defzz-create@users.noreply.github.com>
2025-10-06 19:35:20 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const wheel = document.getElementById("wheel");
-  const spinBtn = document.getElementById("spinBtn");
-
-  spinBtn.addEventListener("click", () => {
-    alert("Здесь будет вращение колеса");
-  });
-});
